a2623857a5fbd0f851d9c8863286adcba3f9c841
Frank <50495924+Bae26@users.noreply.github.com>
2020-07-12 19:55:12 -0700

    changed class to function since class is not needed for redux. Also added counter and log to render on web

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-export default class App extends React.Component {
+export default function App() {
 
-  render() {
-    return (
-      <div>
-        <h1>Succesfully Connected</h1>
-      </div>
-    );
-  }
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  return (
+    <div>
+      <h1>Counter {counter}</h1>
+
+      {isLogged ? <h3>the secret is true</h3> : <h3>the secret is false</h3>}
+    </div>
+  );
+
 }
