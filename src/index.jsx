96bed94a1b62c1328e1f73bee31ab29af60ad26b
Frank <50495924+Bae26@users.noreply.github.com>
2020-07-12 19:42:46 -0700

    combined all reducers and added a store with a provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
+import allReducer from './reducers';
+import { Provider } from 'react-redux';
 
-const store = createStore();
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
