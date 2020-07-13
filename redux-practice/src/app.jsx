b12f2dea43ebdcd1dc64510aa39bcaa15a309cd3
Frank <50495924+Bae26@users.noreply.github.com>
2020-07-13 11:17:12 -0700

    moved folder

--- redux-practice/src/app.jsx
+++ redux-practice/src/app.jsx
@@ -7,11 +7,15 @@ export default function App() {
   const isLogged = useSelector(state => state.isLogged);
   const dispatch = useDispatch();
 
+  if (counter === 5) {
+    dispatch({ type: 'SIGN_IN' });
+  }
+
   return (
     <div>
       <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(increment(5))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(increment(5))}>+</button>
 
       {isLogged ? <h3>the secret is true</h3> : <h3>the secret is false</h3>}
       <button onClick={() => dispatch(signIn())}>Click</button>
