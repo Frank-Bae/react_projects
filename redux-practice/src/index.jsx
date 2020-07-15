64ca53727df6c77d79ea9a175a7dc55412141b37
Frank <50495924+Bae26@users.noreply.github.com>
2020-07-14 17:06:43 -0700

    added thunker thinking it would solve the issue but still need to look more into how to dispatch one time

--- redux-practice/src/index.jsx
+++ redux-practice/src/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import allReducer from './reducers';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(
-  allReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const store = createStore(allReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
